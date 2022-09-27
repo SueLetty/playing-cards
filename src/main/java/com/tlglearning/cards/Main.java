ea59628d8679de7bc27d40c0b83ed495c22b9cfd
SueLetty <xiuxiang1129@gmail.com>
2022-09-27 13:55:27 -0700

    ColorFirst as static nested inner class in Main.

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -19,15 +19,16 @@ public class Main {
     deck.sort(new ColorFirst());
     System.out.println(deck);
   }
+
   private static class ColorFirst implements Comparator<Card> {
 
     @Override
     public int compare(Card card1, Card card2) {
       int comparison = card1.suit().color().compareTo(card2.suit().color());
-      if(comparison == 0){
+      if (comparison == 0) {
         comparison = card1.suit().compareTo(card2.suit());
-        if(comparison == 0){
-          comparison = - card1.rank().compareTo(card2.rank());
+        if (comparison == 0) {
+          comparison = -card1.rank().compareTo(card2.rank());
         }
       }
       return comparison;
