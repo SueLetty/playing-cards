2561c162322589f55535ba5352e916cb296694f9
SueLetty <xiuxiang1129@gmail.com>
2022-09-27 13:22:39 -0700

    Added ColorFirst class. Added two sort methods. Added inner enum Color in Suit enum class.

--- src/main/java/com/tlglearning/cards/model/Deck.java
+++ src/main/java/com/tlglearning/cards/model/Deck.java
@@ -2,6 +2,7 @@ package com.tlglearning.cards.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
@@ -30,6 +31,15 @@ public class Deck implements Iterable<Card>{
   }
   public void shuffle(){
     Collections.shuffle(cards);
+  }
+
+  public void sort(){
+    cards.sort(null);
+
+  }
+  public void sort(Comparator<Card> comparator){
+    cards.sort(comparator);
+
   }
 
   @Override
